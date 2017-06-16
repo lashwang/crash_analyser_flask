3124a8fa478adb281035ac2b1046ba588a44cd7b
simon <lashwang@outlook.com>
2017-06-16 15:19:37 +0800

    use unittest instead of unitest2

--- tests/test_obj_finder.py
+++ tests/test_obj_finder.py
@@ -1,12 +1,12 @@
-import unittest2
+import unittest
 from obj_finder import ObjFinder
 
 
 
-class MyTestCase(unittest2.TestCase):
+class MyTestCase(unittest.TestCase):
     def test_something(self):
         obj_finder = ObjFinder(version_code='700504862',index_file='test.json')
 
 
 if __name__ == '__main__':
-    unittest2.main()
+    unittest.main()
