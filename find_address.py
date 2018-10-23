ff1e6b247eda259fbafbee1f129393c37d9f8999
simon <lashwang@outlook.com>
2018-10-23 15:27:07 +0800

    update

--- find_address.py
+++ find_address.py
@@ -3,17 +3,26 @@
 
 import fire
 import commands
+import os
+import tarfile
 
 brand_job_path = ''
 next_build_number = 0
 
+CACHE_FOLDER = 'caches'
+
+
 def init(version,brand,lib,address):
     global brand_job_path
     global next_build_number
+    global CACHE_FOLDER
+
+
     brand_job_path = '/jenkins_jobs/jobs/{}'.format(brand)
     next_build_number = int(
         commands.getstatusoutput('cat {}/nextBuildNumber'.format(brand_job_path,))[1])
 
+    CACHE_FOLDER = 'caches_{}'.format(brand)
 
 
 #find /jenkins_jobs/jobs/adclear_4_0/builds/226/archive/adclear/build/outputs/apk/ -name "*.apk"
@@ -31,12 +40,33 @@ def find_build_number_by_version(version):
     return build_number_list
 
 
+def copy_lib_file_to_local(build_number_list):
+    if not os.path.exists(CACHE_FOLDER):
+        os.mkdir(CACHE_FOLDER)
+
+
+
+    # /jenkins_jobs/jobs/adclear_4_0/builds/1/archive/adclear/build/outputs/proxy_objs.tar.bz2
+    for build_number in build_number_list:
+        local_build_path = os.path.join(CACHE_FOLDER, build_number)
+        if not os.path.exists(local_build_path):
+            os.mkdir(local_build_path)
+        obj_path = '{}/builds/{}/archive/adclear/build/outputs/proxy_objs.tar.bz2'.format(brand_job_path,build_number)
+        # extract proxy file
+        with tarfile.open(obj_path, 'r:bz2') as tar:
+            subdir_and_files = [
+                tarinfo for tarinfo in tar.getmembers()
+                if 'libproxy.so' in tarinfo.name
+            ]
+            tar.extractall(members=subdir_and_files,path=local_build_path)
+
+
 
 def find_address(version='508594',brand='adclear_4_0',lib='proxy',address='0x1bfd44'):
     print "find_address for address={},version={},brand={},libname={}".format(address,version,brand,lib)
     init(version,brand,lib,address)
     build_number_list = find_build_number_by_version(version)
-
+    copy_lib_file_to_local(build_number_list)
     pass
 
 if __name__ == '__main__':
