2b5c277f6f2436a7d9ec5f4e49d1f3d753eea602
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-16 18:56:48 +0000

    Adding tarfile member sanitization to extractall()

--- test_obj_finder.py
+++ test_obj_finder.py
@@ -33,7 +33,29 @@ class MyTestCase(unittest.TestCase):
                 tarinfo for tarinfo in tar.getmembers()
                 if 'proxy/src/main/obj/local/armeabi/libproxy.so' == tarinfo.name
             ]
-            tar.extractall(members=subdir_and_files)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, members=subdir_and_files)
 
     def test_full_stack_logs(self):
         ObjFinder.query_full_stack_logs(test_stack_logs)
